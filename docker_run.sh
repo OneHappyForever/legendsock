8b9238ae05707fe57a2d20b167743f45549fb8e3
Josh <OneHappyForever@users.noreply.github.com>
2019-01-31 20:06:10 +0800

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -13,13 +13,6 @@ sed -i "s#"_PASSWORD_"#"$MYSQL_PASSWORD"#g" /legendsock/usermysql.json;
 sed -i "s#"_HOSTNAME_"#"$MYSQL_HOST"#g" /legendsock/usermysql.json;
 sed -i "s#"_PORT_"#"$MYSQL_PORT"#g" /legendsock/usermysql.json;
 
-sed -ri "s@^(.*\"timeout\": ).*@\1$TCP_TIMEOUT,@" /legendsock/user-config.json
-sed -ri "s@^(.*\"udp_timeout\": ).*@\1$UDP_TIMEOUT,@" /legendsock/user-config.json
-sed -ri "s@^(.*\"protocol_param\": ).*@\1\"$PROTOCOL_PARAM\",@" /legendsock/user-config.json
-sed -ri "s@^(.*\"speed_limit_per_con\": ).*@\1$SPEED_LIMIT_PER_CON,@" /legendsock/user-config.json
-sed -ri "s@^(.*\"speed_limit_per_user\": ).*@\1$SPEED_LIMIT_PER_USER,@" /legendsock/user-config.json
-sed -ri "s@^(.*\"redirect\": ).*@\1\"${MYSQL_HOST}:443\",@" /legendsock/user-config.json
-
 echo $DOCKER_DNS > /legendsock/dns.conf
 
 exec python /legendsock/server.py m
