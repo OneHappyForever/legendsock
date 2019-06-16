0c1fbf616e5274790418ace2d75b95abca848221
Josh <OneHappyForever@users.noreply.github.com>
2019-06-16 23:33:12 +0800

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -15,4 +15,4 @@ sed -i "s#"_PORT_"#"$MYSQL_PORT"#g" /usr/local/legendsock/usermysql.json;
 
 echo $DOCKER_DNS > /usr/local/legendsock/dns.conf
 
-exec /usr/bin/legendsock start
+/usr/bin/legendsock start
